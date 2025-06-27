21e8f8d54bca8520524b49f7acdc1169d6926e38
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-06-27 15:10:27 -0300

    feat: definir idade e verificar se pessoa é maior de idad

---
+++ estudo-02/estudo02/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    maiorIdade(17)
+}
+
+fun maiorIdade(idade: Int){
+    if (idade >=18) {
+        println("Maior de idade")
+    } else {
+        print("Menor de idade")
+    }
+}
